77eb30f2e0b56cf47d39984c2021ab3f9f52849f
Lakshmi Narasimma KG <kglvivek5@gmail.com>
2016-03-04 13:36:49 +0530

    Latest version

--- QuizBeta/ScoreDisplayViewController.m
+++ QuizBeta/ScoreDisplayViewController.m
@@ -7,12 +7,16 @@
 //
 
 #import "ScoreDisplayViewController.h"
+#import <Social/Social.h>
+
 
 @interface ScoreDisplayViewController ()
 
 @property (strong, nonatomic) IBOutlet UILabel *scoreDisplayLabel;
 
 - (IBAction)topicSelectionTapped:(UIButton *)sender;
+
+- (IBAction)shareOptions:(UIButton *)sender;
 
 @end
 
@@ -38,6 +42,24 @@
 
 - (IBAction)topicSelectionTapped:(UIButton *)sender {
     [self performSegueWithIdentifier:@"displayHome" sender:self];
+}
+
+- (IBAction)shareOptions:(UIButton *)sender {
+    UIAlertController *actionSheet = [UIAlertController alertControllerWithTitle:@"Share the Score to your Friends!!" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    [actionSheet addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        // do nothing;
+    }]];
+    [actionSheet addAction:[UIAlertAction actionWithTitle:@"Facebook" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        // share to Facebook
+    }]];
+    [actionSheet addAction:[UIAlertAction actionWithTitle:@"Twitter" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]) {
+            
+        } else {
+            NSLog(@"Twitter account is not available in your phone");
+        }
+    }]];
+    [self presentViewController:actionSheet animated:YES completion:nil];
 }
 
 
